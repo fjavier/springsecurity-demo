564735d9185babb9265c25022107d0aed6a4e002
Francisco Briceño S <fbricenos@applaudostudios.com>
2023-04-21 20:03:12 -0600

    Added userDetailService to set  user and password

--- src/main/java/dev/fbricenos/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/dev/fbricenos/springsecuritydemo/config/SecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,10 +18,23 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
               .authorizeRequests(authorization -> {
-                  authorization.requestMatchers("api/v1/demo/private").authenticated();
                   authorization.requestMatchers("api/v1/demo/public").permitAll();
+                  authorization.requestMatchers("/error").permitAll();
+                  authorization.requestMatchers("/favicon.ico").permitAll();
+                  authorization.anyRequest().authenticated();
               })
                 .formLogin(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService getUserDetailsService(){
+        return new InMemoryUserDetailsManager(
+                User.builder()
+                      .username("user")
+                      .password("{noop}password")
+                      .roles("USER")
+                      .build()
+        );
     }
 }
